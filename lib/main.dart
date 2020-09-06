a34bfffb92235ac6af976d0f566e5567e7bd4efa
Damodar Lohani <dlohani48@gmail.com>
2020-09-06 20:25:40 +0545

    recent search one item in home page if not anything resulted with error

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Dictio',
-      darkTheme: ThemeData.dark().copyWith(),
+      darkTheme: ThemeData.dark().copyWith(
+        accentColor: Colors.red,
+      ),
       theme: ThemeData(
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -55,11 +57,13 @@ final searchControllerProvider =
 
 final searchResultProvider = StateProvider<OwlBotResponse>((ref) => null);
 final loadingProvider = StateProvider<bool>((ref) => false);
+final errorProvider = StateProvider<String>((ref) => '');
 
 class HomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, watch) {
     final searchResult = watch(searchResultProvider).state;
+    final error = watch(errorProvider).state;
     return Scaffold(
       appBar: AppBar(
         title: TextField(
@@ -76,7 +80,9 @@ class HomePage extends ConsumerWidget {
         ),
         actions: [
           IconButton(
-            icon: watch(loadingProvider).state ? CircularProgressIndicator() :  Icon(Icons.search),
+            icon: watch(loadingProvider).state
+                ? CircularProgressIndicator()
+                : Icon(Icons.search),
             onPressed: () {
               _search(context);
             },
@@ -86,8 +92,38 @@ class HomePage extends ConsumerWidget {
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: <Widget>[
+          if (error != null && error.isNotEmpty) ...[
+            Card(
+              child: ListTile(
+                title: Text(watch(searchControllerProvider).state.text ?? ''),
+                subtitle: Text(error),
+                trailing: Icon(
+                  Icons.error,
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+          ],
           if (searchResult != null)
-            DictionaryListItem(dictionaryItem: searchResult)
+            DictionaryListItem(dictionaryItem: searchResult),
+          if (searchResult == null) ...[
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 8.0,
+                bottom: 4.0,
+              ),
+              child: Text(
+                "Recent search",
+                style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                ),
+              ),
+            ),
+            DictionaryListItem(
+              dictionaryItem: Hive.box<OwlBotResponse>(historyBox).getAt(0),
+            ),
+          ],
         ],
       ),
       bottomNavigationBar: BottomAppBar(
@@ -116,21 +152,25 @@ class HomePage extends ConsumerWidget {
 
   _search(BuildContext context) async {
     final query = context.read(searchControllerProvider).state.text;
-    if(query == null || query.isEmpty || context.read(loadingProvider).state) return;
+    if (query == null || query.isEmpty || context.read(loadingProvider).state)
+      return;
+    context.read(errorProvider).state = '';
     context.read(loadingProvider).state = true;
     FocusScope.of(context).requestFocus(FocusNode());
     final box = Hive.box<OwlBotResponse>(historyBox);
     if (box.containsKey(query)) {
       context.read(searchResultProvider).state = box.get(query);
+      context.read(loadingProvider).state = false;
       return;
     }
-    if (query != null && query.isNotEmpty) {
-      final res = await OwlBot(token: TOKEN).define(word: query);
-      if (res != null) {
-        box.put(res.word, res);
-      }
-      context.read(searchResultProvider).state = res;
+
+    final res = await OwlBot(token: TOKEN).define(word: query);
+    if (res != null) {
+      box.put(res.word, res);
+    } else {
+      context.read(errorProvider).state = '404 Not found';
     }
+    context.read(searchResultProvider).state = res;
     context.read(loadingProvider).state = false;
   }
 }
@@ -149,82 +189,85 @@ class DictionaryListItem extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Card(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(12.0, 8.0, 0.0, 8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        dictionaryItem.word,
-                        style: Theme.of(context).textTheme.headline5,
-                      ),
-                      if (dictionaryItem.pronunciation != null) ...[
-                        const SizedBox(height: 5.0),
-                        Text(dictionaryItem.pronunciation),
-                      ],
-                    ],
-                  ),
-                ),
-                ValueListenableBuilder(
-                  valueListenable:
-                      Hive.box<OwlBotResponse>(favoritesBox).listenable(),
-                  builder: (context, box, child) => IconButton(
-                    icon: Icon(
-                      Icons.star,
-                      color: box.containsKey(dictionaryItem.word)
-                          ? Colors.deepOrange
-                          : null,
-                    ),
-                    onPressed: () {
-                      if (box.containsKey(dictionaryItem.word)) {
-                        box.delete(dictionaryItem.word);
-                      } else {
-                        box.put(dictionaryItem.word, dictionaryItem);
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ...dictionaryItem.definitions.map(
-                  (e) => Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          e.type,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 8.0, 0.0, 8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            dictionaryItem.word,
+                            style: Theme.of(context).textTheme.headline5,
                           ),
-                        ),
-                        const SizedBox(height: 5.0),
-                        Text(e.definition),
-                        if (e.example != null) ...[
-                          const SizedBox(height: 5.0),
-                          Text("Example: ${e.example}"),
+                          if (dictionaryItem.pronunciation != null) ...[
+                            const SizedBox(height: 5.0),
+                            Text(dictionaryItem.pronunciation),
+                          ],
                         ],
-                        if (e.imageUrl != null) ...[
-                          const SizedBox(height: 10.0),
-                          Image.network(e.imageUrl),
-                        ],
-                      ],
+                      ),
                     ),
-                  ),
+                    ValueListenableBuilder(
+                      valueListenable:
+                          Hive.box<OwlBotResponse>(favoritesBox).listenable(),
+                      builder: (context, box, child) => IconButton(
+                        icon: Icon(
+                          Icons.star,
+                          color: box.containsKey(dictionaryItem.word)
+                              ? Colors.deepOrange
+                              : null,
+                        ),
+                        onPressed: () {
+                          if (box.containsKey(dictionaryItem.word)) {
+                            box.delete(dictionaryItem.word);
+                          } else {
+                            box.put(dictionaryItem.word, dictionaryItem);
+                          }
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Divider(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ...dictionaryItem.definitions.map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              e.type,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 5.0),
+                            Text(e.definition),
+                            if (e.example != null) ...[
+                              const SizedBox(height: 5.0),
+                              Text("Example: ${e.example}"),
+                            ],
+                            if (e.imageUrl != null) ...[
+                              const SizedBox(height: 10.0),
+                              Image.network(e.imageUrl),
+                            ],
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ],
