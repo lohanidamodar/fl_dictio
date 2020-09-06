4527dfe5c2f726ea8926f49e2e850eff7bfe7d5a
Damodar Lohani <dlohani48@gmail.com>
2020-09-06 06:44:04 +0545

    owlbot response hive adapter

---
+++ lib/owlbot_res_adapter.dart
@@ -0,0 +1,50 @@
+import 'package:hive/hive.dart';
+import 'package:owlbot_dart/owlbot_dart.dart';
+
+class OwlBotResAdapter extends TypeAdapter<OwlBotResponse> {
+  @override
+  OwlBotResponse read(BinaryReader reader) {
+    Map<String, dynamic> data = {
+      "word": reader.readString(),
+      "pronounciation": reader.readString(),
+      "definitions": reader.readList(),
+    };
+    return OwlBotResponse.fromJson(data);
+  }
+
+  @override
+  int get typeId => 2;
+
+  @override
+  void write(BinaryWriter writer, OwlBotResponse obj) {
+    writer.writeString(obj.word);
+    writer.writeString(obj.pronunciation);
+    writer.writeList(obj.definitions);
+  }
+}
+
+class OwlBotDefinitionAdapter extends TypeAdapter<OwlBotDefinition> {
+  @override
+  OwlBotDefinition read(BinaryReader reader) {
+    Map<String, dynamic> data = {
+      "definition": reader.readString(),
+      "type": reader.readString(),
+      "emoji": reader.readString(),
+      "example": reader.readString(),
+      "image_url": reader.readString(),
+    };
+    return OwlBotDefinition.fromJson(data);
+  }
+
+  @override
+  int get typeId => 1;
+
+  @override
+  void write(BinaryWriter writer, OwlBotDefinition obj) {
+    writer.writeString(obj.definition);
+    writer.writeString(obj.type);
+    writer.writeString(obj.emoji);
+    writer.writeString(obj.example);
+    writer.writeString(obj.imageUrl);
+  }
+}
